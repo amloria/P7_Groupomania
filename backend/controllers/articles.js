ac32901bfba427c319609c9d3261a0badc3fa5a8
Agustin Loria <agustin.loria93@gmail.com>
2022-08-12 12:12:02 +0200

    ud220812

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -3,11 +3,9 @@ const fs = require("fs");
 
 exports.createArticle = (req, res, next) => {
   const articleObject = JSON.parse(req.body.article);
-  delete articleObject._id;
-  delete articleObject._userId;
   const article = new Article({
     ...articleObject,
-    userId: req.auth.userId,
+    // userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
@@ -19,6 +17,16 @@ exports.createArticle = (req, res, next) => {
     })
     .catch((error) => {
       res.status(400).json({ error });
+    });
+};
+
+exports.getOneArticle = (req, res, next) => {
+  Article.findOne({ _id: req.params.id })
+    .then((article) => {
+      res.status(200).json(article);
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
     });
 };
 
@@ -42,12 +50,11 @@ exports.modifyArticle = (req, res, next) => {
       }
     : { ...req.body };
 
-  delete articleObject._userId;
   Article.findOne({ _id: req.params.id })
     .then((article) => {
-      if (article.userId != req.auth.userId) {
-        res.status(403).json({ message: "Not authorized" });
-      } else {
+      // if (article.userId != req.auth.userId) {
+      //   res.status(403).json({ message: "Not authorized" });
+      // } else {
         Article.updateOne(
           { _id: req.params.id },
           { ...articleObject, _id: req.params.id }
@@ -56,7 +63,7 @@ exports.modifyArticle = (req, res, next) => {
             res.status(200).json({ message: "Updated successfully!" })
           )
           .catch((error) => res.status(401).json({ error }));
-      }
+      // }
     })
     .catch((error) => {
       res.status(400).json({ error });
@@ -66,9 +73,9 @@ exports.modifyArticle = (req, res, next) => {
 exports.deleteArticle = (req, res, next) => {
   Article.findOne({ _id: req.params.id })
     .then((article) => {
-      if (article.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
-      } else {
+      // if (article.userId != req.auth.userId) {
+      //   res.status(401).json({ message: "Not authorized" });
+      // } else {
         const filename = article.imageUrl.split("/images/")[1];
         fs.unlink(`images/${filename}`, () => {
           Article.deleteOne({ _id: req.params.id })
@@ -77,7 +84,7 @@ exports.deleteArticle = (req, res, next) => {
             })
             .catch((error) => res.status(401).json({ error }));
         });
-      }
+      // }
     })
     .catch((error) => {
       res.status(500).json({ error });
--- backend/models/Article.js
+++ backend/models/Article.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const articleSchema = mongoose.Schema({
   userId: { type: String, required: true },
   description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   usersLiked: [{ type: String }],
