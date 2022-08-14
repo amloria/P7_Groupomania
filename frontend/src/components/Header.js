60531fea2477278ffdc20af7066730084ca52c49
Agustin Loria <agustin.loria93@gmail.com>
2022-08-14 11:50:31 +0200

    ud220814

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,23 +7,25 @@ import logo from "../assets/icon-left-font.png";
 function Header() {
   return (
     <>
-      <header className="header-nav">
-        <div className="header-logo-search">
-          <Link to="/feed">
-            <img src={logo} className="header-logo" alt="Logo Groupomania" />
-          </Link>
-        </div>
-        <div>
-          <input
-            className="header-search"
-            placeholder="Rechercher"
-            aria-label="Rechercher"
-            type="text"
-          ></input>
-        </div>
-        <div className="header-profile">
-          <Link to="/profile">Profil</Link>
-        </div>
+      <header>
+        <nav className="header-nav">
+          <div className="header-logo-search">
+            <Link to="/feed">
+              <img src={logo} className="header-logo" alt="Logo Groupomania" />
+            </Link>
+          </div>
+          <div>
+            <input
+              className="header-search"
+              placeholder="Rechercher"
+              aria-label="Rechercher"
+              type="text"
+            ></input>
+          </div>
+          <div className="header-profile">
+            <Link to="/profile">Profil</Link>
+          </div>
+        </nav>
       </header>
       <Outlet />
     </>
