d83cea3c189f28542127d5510aed80b075321d85
Agustin Loria <agustin.loria93@gmail.com>
2022-09-07 13:44:45 +0200

    ud220907

--- frontend/src/components/CreateArticle.js
+++ frontend/src/components/CreateArticle.js
@@ -1,81 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/CreateArticle.css";
 
 function CreateArticle() {
-  function formArticle() {
-    return (
-      <div className="new-post">
-        <div>
-          <a href="#" className="user-avatar">
-            AV
-          </a>
-          <form>
-            <input
-              type="text"
-              className="create-post"
-              placeholder="Description..."
-            >
-              <input>
-                <i className="fa-regular fa-lg fa-image"></i>
-              </input>
-            </input>
-          </form>
-        </div>
-      </div>
-    );
-  }
+  const [newArticle, setNewArticle] = useState(false);
 
-  return (
-    <>
-      {/* <div className="new-post">
-        <div>
-          <a href="#" className="user-avatar">
-            AV
-          </a>
-          <button
-            className="create-post"
-            title="Créer une nouvelle publication"
-            onClick={formArticle}
-          >
-            <span>Salut, quoi de neuf ?</span>
-            <i className="fa-regular fa-lg fa-image"></i>
-          </button>
-        </div>
-      </div> */}
-      <div className="new-post-container">
-        <div className="container-top">
-          <h2>Créer une publication</h2>
-          <i className="cancel-post fa-solid fa-lg fa-xmark"></i>
-        </div>
-        <div className="container-user">
-          <a href="#" className="user-avatar">
-              AV
-          </a>
-          <span className="user-name">
-                User
-          </span>
-        </div>      
-        <form className="new-post-form">
-          <div>
-            <div className="new-post-description">
-              <input
-                className="create-comment"
-                placeholder="Ajouter une description..."
-                aria-label="Ajouter une description"
-                type="text"
-              ></input>
-            </div>
-          </div>
-          <div className="container-bottom">
-            <div className="add-image" title="Ajouter une image">
-              <i className="fa-regular fa-lg fa-image"></i>
-            </div>
-            <button className="share-post">Créer</button>
-          </div>
-        </form>
+  return newArticle ? (
+    <div className="new-post">
+      <div>
+        <a href="#" className="user-avatar">
+          AV
+        </a>
+        <button
+          className="create-post"
+          title="Créer une nouvelle publication"
+          onClick={() => setNewArticle(false)}
+        >
+          <span>Salut, quoi de neuf ?</span>
+          <i className="fa-regular fa-lg fa-image"></i>
+        </button>
       </div>
-    </>
+    </div>
+  ) : (
+    <div className="new-post-container">
+      <div className="container-top">
+        <h2>Créer une publication</h2>
+        <button className="cancel-post" onClick={() => setNewArticle(true)}>
+          <i className="fa-solid fa-lg fa-xmark"></i>
+        </button>
+      </div>
+      <div className="container-user">
+        <a href="#" className="user-avatar">
+          AV
+        </a>
+        <span className="user-name">User</span>
+      </div>
+      <form className="new-post-form">
+        <div>
+          <div className="new-post-description">
+            <input
+              className="create-comment"
+              name="description"
+              id="post-description"
+              placeholder="Ajouter une description..."
+              aria-label="Ajouter une description"
+              type="text"
+            ></input>
+          </div>
+        </div>
+        <div className="container-bottom">
+          <div className="add-content">
+            <i
+              className="face-smile fa-regular fa-lg fa-face-smile-beam"
+              title="Ajouter des emojis"
+            ></i>
+            <label for="file">
+              <i
+                className="add-image fa-regular fa-lg fa-image"
+                title="Ajouter une image"
+              ></i>
+            </label>
+            <input
+              id="file"
+              className="input-image"
+              type="file"
+              accept="image/jpeg, image/jpg, image/png"
+            ></input>
+          </div>
+          <button className="share-post">Publier</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
